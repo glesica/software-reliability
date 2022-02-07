8b43fdd63cdcf868bfa1337f830b47387dd9246c
George Lesica <george@lesica.com>
2022-02-07 09:08:12 -0700

    Rebuild

--- topics/unit-testing/example/math.py
+++ topics/unit-testing/example/math.py
@@ -1,4 +1,4 @@
-def double(x: int):
+def double(x: int) -> int:
     """
     >>> double(0)
     0
@@ -6,4 +6,10 @@ def double(x: int):
     4
     """
     return x * 2
+
+def average(x) -> float:
+    s = x[0]
+    for v in x[1:]:
+        s += v
+    return s / (len(x) - 1)
 
